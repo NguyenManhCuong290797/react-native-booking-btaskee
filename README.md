b2d7709f903854d7dfb6941e90e410f2f90c6fee
bTaskee Dev <btaskeedev@Phuongs-MacBook-Pro.local>
2023-03-06 15:00:11 +0700

    commit

--- README.md
+++ README.md
@@ -1,275 +1,34 @@
-|                          | |  |   |   |
-| --------------------------------------- | -------- | ---------- |---------- |---------- |
-| <a href="https://www.npmjs.com/package/react-native-dropdown-select-list">![NPM VERSION](https://img.shields.io/npm/v/react-native-dropdown-select-list?style=for-the-badge)</a> | <a href="https://www.npmjs.com/package/react-native-dropdown-select-list">![NPM WEEKLY DOWNLOADS](https://img.shields.io/npm/dw/react-native-dropdown-select-list?color=%232CA215&label=WEEKLY%20DOWNLOADS&style=for-the-badge)</a> | <a href="https://github.com/danish1658/react-native-dropdown-select-list/stargazers">![GITHUB STAR](https://img.shields.io/github/stars/danish1658/react-native-dropdown-select-list?label=Give%20Us%20A%20Star&style=for-the-badge)</a> | <a href="https://www.youtube.com/channel/UCEbbpzmnbRiNVhJ3ElABbMQ">![YOUTUBE VIEWS](https://img.shields.io/youtube/channel/views/UCEbbpzmnbRiNVhJ3ElABbMQ?label=YOUTUBE%20VIEWS&style=for-the-badge)</a> | <a href="https://www.npmjs.com/package/react-native-dropdown-select-list">![NPM LIFETIME DOWNLOADS](https://img.shields.io/npm/dt/react-native-dropdown-select-list?color=%232CA215&style=for-the-badge)</a>
+# React Native Dropdown Picker 5.x
 
-<br>
-<h1 align="center">
-  ⭐ React Native Dropdown Select List </h1>
-
-<div align="center">
-
-React Native Select List Equivalent to Html's Select "
-
-</div>
-
-<p align="center" >
-  <div style="display:flex;justify-content:space-evenly;align-items:center">
-    
-Multiple Select List            |  Select List
-:-------------------------:|:-------------------------:
-![](https://i.imgur.com/EAT81Nz.gif)  |  ![](https://i.imgur.com/qmHreFH.gif)
-    
-  </div>
-  <br>
-  
+<p float="left">
+    <img src="https://user-images.githubusercontent.com/56504893/116790110-e0b36880-aac7-11eb-9ebd-196acee64f7a.png" width="270" alt="Screenshot">
+    <img src="https://user-images.githubusercontent.com/56504893/116789802-faec4700-aac5-11eb-837b-86f18cbfcf3d.png" width="270" alt="Screenshot">
+    <img src="https://user-images.githubusercontent.com/56504893/116789839-2c651280-aac6-11eb-99e0-b43b608ed8c7.png" width="270" alt="Screenshot">
 </p>
-<br>
 
-<h4>Light weight and <b>Easy</b> to use dropdown select list.</h4>
+The example in the screenshots: https://snack.expo.dev/8mHmLfcZf
 
--   Style it your way with style props of every view.
--   Smooth performance on all platforms IOS, Android and Web.
--   Change Font Family Easily which community picker lacks.
--   <b>Zero</b> dependencies
+# Documentation
+**Visit https://hossein-zare.github.io/react-native-dropdown-picker-website/**
 
-<br>
+# Merge and Release Process
 
-# Compatibility
+## Branches in use
 
-<br>
+### Development
 
-|  iOS  | Android | Web | Expo |
---------|---------|-----|------|
-|  ✅  |    ✅    | ✅ |  ✅  |
+PRs should be made against and merged into the [`dev-5.x`](https://github.com/hossein-zare/react-native-dropdown-picker) branch, which is set as the `default` branch on github.
 
+### Release
 
-<br>
+Releases are currently made from the [`5.x`](https://github.com/hossein-zare/react-native-dropdown-picker/tree/5.x) branch.
 
-# 🔌 Installation
+## Release Process
 
-```sh
-$ npm install react-native-dropdown-select-list
+To make a new release, follow these steps:
 
-```
-
-OR
-
-```sh
-$ yarn add react-native-dropdown-select-list
-```
-
-<br>
-
-# 😎 Basic Usage for SelectList
-```jsx
-import { SelectList } from 'react-native-dropdown-select-list'
-
-const App = () => {
-
-  const [selected, setSelected] = React.useState("");
-  
-  const data = [
-      {key:'1', value:'Mobiles', disabled:true},
-      {key:'2', value:'Appliances'},
-      {key:'3', value:'Cameras'},
-      {key:'4', value:'Computers', disabled:true},
-      {key:'5', value:'Vegetables'},
-      {key:'6', value:'Diary Products'},
-      {key:'7', value:'Drinks'},
-  ]
-
-  return(
-    <SelectList 
-        setSelected={(val) => setSelected(val)} 
-        data={data} 
-        save="value"
-    />
-  )
-
-};
-```
-<br>
-
-# 😎 Basic Usage for MultipleSelectList
-```jsx
-import { MultipleSelectList } from 'react-native-dropdown-select-list'
-
-const App = () => {
-
-  const [selected, setSelected] = React.useState([]);
-  
-  const data = [
-      {key:'1', value:'Mobiles', disabled:true},
-      {key:'2', value:'Appliances'},
-      {key:'3', value:'Cameras'},
-      {key:'4', value:'Computers', disabled:true},
-      {key:'5', value:'Vegetables'},
-      {key:'6', value:'Diary Products'},
-      {key:'7', value:'Drinks'},
-  ]
-
-  return(
-    <MultipleSelectList 
-        setSelected={(val) => setSelected(val)} 
-        data={data} 
-        save="value"
-        onSelect={() => alert(selected)} 
-        label="Categories"
-    />
-  )
-
-};
-```
-
-<br>
-
-
-For Live `Demo` [(Expo Snack)](https://snack.expo.dev/@danish1658/react-native-dropdown-select-list)
-
-<br>
-
-# 🔧 General Props
-<i>Applicable on both <b>SelectList</b> & <b>MultipleSelectList</b> Components</i>
-
-| Name | Type | Description |
-| ---- | ---- | ----------- |
-| save| string | Pass ('key' or 'value') to save data of your choice in your local state variable
-| onSelect| Function | Pass any function that you want to trigger immediately after a value is selected
-| placeholder | String | Placeholder text that will be displayed in the select box
-| search | boolean | set to false if you dont want to use search functionality
-| maxHeight| Number | Maximum height of the dropdown wrapper to occupy
-| data| array or array[object]| Data which will be iterated as options of select list
-| setSelected| Function | For Setting the option value which will be stored in your local state
-| searchicon| JSX Element | Pass any JSX to this prop like Text, Image or Icon to show instead of search icon
-| arrowicon| JSX Element | Pass any JSX to this prop like Text, Image or Icon to show instead of chevron icon
-| closeicon| JSX Element | Pass any JSX to this prop like Text, Image or Icon to show instead of close icon
-| searchPlaceholder| String | Custom placeholder text for search TextInput
-| defaultOption| Object | Pass default selected option in key value pair
-| fontFamily| string | Pass font name to apply globally on each text field of component
-| notFoundText| string | Pass your custom message if any search result returns empty
-| dropdownShown| boolean | Control your dropdown ( on & off ) state by changing its value to true or false
-
-<br>
-
-# 🔧 General Style Props
-<i>Applicable on both <b>SelectList</b> & <b>MultipleSelectList</b> Components</i>
-
-| Name | Type | Description |
-| ---- | ---- | ----------- |
-| boxStyles| Object| Additional styles for select box parent wrapper
-| inputStyles| Object| Additional styles for text of selection box
-| dropdownStyles| Object| Additional styles for dropdown scrollview
-| dropdownItemStyles| Object| Additional styles for dropdown single list item
-| dropdownTextStyles| Object| Additional styles for dropdown list items text
-| disabledItemStyles| Object| Additional styles for disabled dropdown list item
-| disabledTextStyles| Object| Additional styles for disabled dropdown list items text
-
-<br>
-
-# 😎 Advanced Usage
-```jsx
-import { SelectList } from 'react-native-dropdown-select-list'
-
-const App = () => {
-
-  const [selected, setSelected] = React.useState("");
-  
-  const data = [
-    {key:'1',value:'Jammu & Kashmir'},
-    {key:'2',value:'Gujrat'},
-    {key:'3',value:'Maharashtra'},
-    {key:'4',value:'Goa'},
-  ];
-
-  return(
-    <SelectList 
-      onSelect={() => alert(selected)}
-      setSelected={setSelected} 
-      fontFamily='lato'
-      data={data}  
-      arrowicon={<FontAwesome name="chevron-down" size={12} color={'black'} />} 
-      searchicon={<FontAwesome name="search" size={12} color={'black'} />} 
-      search={false} 
-      boxStyles={{borderRadius:0}} //override default styles
-      defaultOption={{ key:'1', value:'Jammu & Kashmir' }}   //default selected option
-    />
-  )
-
-};
-```
-
-<br>
-
-# 😎 Getting Options From Database
-```jsx
-import { SelectList } from 'react-native-dropdown-select-list'
-
-const App = () => {
-
-  const [selected, setSelected] = React.useState("");
-  const [data,setData] = React.useState([]);
-  
-  React.useEffect(() => 
-    //Get Values from database
-    axios.get('https://jsonplaceholder.typicode.com/users')
-      .then((response) => {
-        // Store Values in Temporary Array
-        let newArray = response.data.map((item) => {
-          return {key: item.id, value: item.name}
-        })
-        //Set Data Variable
-        setData(newArray)
-      })
-      .catch((e) => {
-        console.log(e)
-      })
-  ,[])
-
-  return(
-    <SelectList setSelected={setSelected} data={data} onSelect={() => alert(selected)} />
-  )
-
-};
-```
-
-<br>
-
-# 🔧 Additional Props
-<i>Applicable on <b>MultipleSelectList</b> Only</i>
-
-| Name | Type | Description |
-| ---- | ---- | ----------- |
-| label| string| Pass any string to be placed instead of default label
-
-
-<br>
-
-# 🔧 Additional Style Props
-<i>Applicable on <b>MultipleSelectList</b> Only</i>
-
-| Name | Type | Description |
-| ---- | ---- | ----------- |
-| disabledCheckBoxStyles| Object| Additional styles disabled checkbox inside dropdown
-| checkBoxStyles| Object| Additional styles for active checkbox
-| badgeStyles| Object| Additional styles for badges of selected values
-| badgeTextStyles| Object| Additional styles for Text of badges of selected values
-| labelStyles| Object| Additional styles for label of multiple select list
-
-<br>
-
-
-# ▶️ Watch Video
-
-[![Watch the video](https://i.imgur.com/K8Lt2h4.png)](https://www.youtube.com/watch?v=J9raEY-1KPQ&t=499s)
-
-<br>
-
-# 💲 Would you like to support me?
-
-If you would like me come up with similar packages, buy me a cup of coffee to boost my energy.
-<br><br>
-[![Paypal](https://www.paypalobjects.com/webstatic/mktg/Logo/pp-logo-100px.png)](https://paypal.me/danishamindar)
-<br><br>
-For Live `Demo` [(Expo Snack)](https://snack.expo.dev/@danish1658/react-native-dropdown-select-list)
-
-<br>
+* Verify the development branch has all the changes desired in a release and works well
+* Make and merge a final PR into development branch that increments the version number in `package.json`
+* Make and merge a PR from the development branch to the release branch
+* Using the GitHub web UI, draft a new release using tag name `vx.x.x` (replace the `x` values as appropriate of course), with the release branch as the target, with release name `vx.x.x` (again, with appropriate numbers in place of `x` of course)
+* Verify in the GitHub Actions panel for the repository that NPM publish succeeded
--- index.d.ts
+++ index.d.ts
@@ -1,269 +1,328 @@
+declare module 'react-native-dropdown-picker' {
+  import type { Dispatch, PropsWithoutRef } from 'react';
+  import type {
+    FlatListProps,
+    LayoutChangeEvent,
+    ModalProps,
+    ScrollViewProps,
+    StyleProp,
+    TextInputProps,
+    TextProps,
+    TextStyle,
+    ViewProps,
+    TouchableOpacityProps,
+    ViewStyle,
+  } from 'react-native';
 
-import type * as React from "react";
-import { ViewStyle, TextStyle } from 'react-native';
+  type SetStateCallback<S> = ((prevState: S) => S);
+  type SetStateValue<S> = ((prevState: S) => S);
 
-export interface SelectListProps  {
-    /**
-    * Fn to set Selected option value which will be stored in your local state
-    */
-    setSelected: Function,
+  export type ValueType = string | number | boolean;
 
-    /**
-    * Placeholder text that will be displayed in the select box
-    */
-    placeholder?: string,
+  export type ItemType<T> = {
+    label?: string;
+    value?: T;
+    icon?: () => JSX.Element;
+    parent?: T;
+    selectable?: boolean;
+    disabled?: boolean;
+    testID?: string;
+    containerStyle?: StyleProp<ViewStyle>;
+    labelStyle?: StyleProp<TextStyle>;
+  };
 
-    /**
-    * Additional styles for select box
-    */
-    boxStyles?: ViewStyle,
+  export type ModeType = 'DEFAULT' | 'SIMPLE' | 'BADGE';
 
-    /**
-    *  	Additional styles for text of select box
-    */
-    inputStyles?: TextStyle,
+  export interface ModeInterface {
+    DEFAULT: string;
+    SIMPLE: string;
+    BADGE: string;
+  }
 
-    /**
-    *  	Additional styles for dropdown scrollview
-    */
-    dropdownStyles?:ViewStyle,
+  export type ListModeType = 'DEFAULT' | 'FLATLIST' | 'SCROLLVIEW' | 'MODAL';
 
-    /**
-    *  Additional styles for dropdown list item
-    */
-    dropdownItemStyles?: ViewStyle,
+  export interface ListModeInterface {
+    DEFAULT: string;
+    FLATLIST: string;
+    SCROLLVIEW: string;
+    MODAL: string;
+  }
 
-    /**
-    * Additional styles for list items text
-    */
-    dropdownTextStyles?: TextStyle,
+  export interface SchemaInterface {
+    label: string;
+    value: string;
+    icon: string;
+    parent: string;
+    selectable: string;
+    disabled: string;
+    testID: string;
+    containerStyle: string;
+    labelStyle: string;
+  }
 
-    /**
-    * Maximum height of the dropdown wrapper to occupy
-    */
-    maxHeight?: number,
+  export type LanguageType =
+    | 'DEFAULT'
+    | 'FALLBACK'
+    | 'EN'
+    | 'AR'
+    | 'FA'
+    | 'TR'
+    | 'RU'
+    | 'ES'
+    | 'ID'
+    | 'IT'
+    | 'PT'
+    | 'FR';
 
-    /**
-    * Data which will be iterated as options of select list
-    */
-    data: Array<{}>,
+  export interface TranslationInterface {
+    PLACEHOLDER: string;
+    SEARCH_PLACEHOLDER: string;
+    SELECTED_ITEMS_COUNT_TEXT: string | {
+      [key in (number | "n")]: string;
+    };
+    NOTHING_TO_SHOW: string;
+  }
 
-    /**
-    * The default option of the select list
-    */
-    defaultOption?: { key: any, value: any },
+  export interface RenderBadgeItemPropsInterface<T> {
+    label: string;
+    value: T;
+    props: TouchableOpacityProps;
+    IconComponent: () => JSX.Element;
+    textStyle: StyleProp<TextStyle>;
+    badgeStyle: StyleProp<ViewStyle>;
+    badgeTextStyle: StyleProp<TextStyle>;
+    badgeDotStyle: StyleProp<ViewStyle>;
+    getBadgeColor: (value: string) => string;
+    getBadgeDotColor: (value: string) => string;
+    showBadgeDot: boolean;
+    onPress: (value: T) => void;
+    rtl: boolean;
+    THEME: ThemeType;
+  }
 
-    /**
-    * Pass any JSX to this prop like Text, Image or Icon to show instead of search icon
-    */
-    searchicon?: JSX.Element,
+  export interface RenderListItemPropsInterface<T> {
+    rtl: boolean;
+    item: ItemType<T>;
+    label: string;
+    value: T;
+    parent: T;
+    selectable: boolean;
+    disabled: boolean;
+    props: ViewProps;
+    custom: boolean;
+    isSelected: boolean;
+    IconComponent: () => JSX.Element;
+    TickIconComponent: () => JSX.Element;
+    listItemContainerStyle: StyleProp<ViewStyle>;
+    listItemLabelStyle: StyleProp<TextStyle>;
+    listChildContainerStyle: StyleProp<ViewStyle>;
+    listParentContainerStyle: StyleProp<ViewStyle>;
+    listChildLabelStyle: StyleProp<TextStyle>;
+    listParentLabelStyle: StyleProp<TextStyle>;
+    customItemContainerStyle: StyleProp<ViewStyle>;
+    customItemLabelStyle: StyleProp<TextStyle>;
+    selectedItemContainerStyle: StyleProp<ViewStyle>;
+    selectedItemLabelStyle: StyleProp<TextStyle>;
+    disabledItemContainerStyle: StyleProp<ViewStyle>;
+    disabledItemLabelStyle: StyleProp<TextStyle>;
+    containerStyle: StyleProp<ViewStyle>;
+    labelStyle: StyleProp<TextStyle>;
+    categorySelectable: boolean;
+    onPress: (value: T) => void;
+    setPosition: (value: T, y: number) => void;
+    theme: ThemeNameType;
+    THEME: ThemeType;
+  }
 
-    /**
-    *  Pass any JSX to this prop like Text, Image or Icon to show instead of chevron icon
-    */
-    arrowicon?: JSX.Element,
+  export interface ActivityIndicatorComponentPropsInterface {
+    size: number;
+    color: string;
+  }
 
-    /**
-    * set to false if you dont want to use search functionality
-    */
-    search?: boolean,
+  export interface ListEmptyComponentPropsInterface {
+    listMessageContainer: StyleProp<ViewStyle>;
+    listMessageTextStyle: StyleProp<TextStyle>;
+    ActivityIndicatorComponent: (
+      props: ActivityIndicatorComponentPropsInterface,
+    ) => JSX.Element;
+    loading: boolean;
+    message: string;
+  }
 
-    /**
-    * set to false if you dont want to use search functionality
-    */
-    searchPlaceholder?: string,
+  export type DropDownDirectionType = 'DEFAULT' | 'TOP' | 'BOTTOM' | 'AUTO';
+  export type ThemeNameType = 'DEFAULT' | 'LIGHT' | 'DARK';
+  export type ThemeType = object;
 
-    /**
-    * Trigger an action when option is selected
-    */
-    onSelect?: () => void,
+  interface DropDownPickerBaseProps<T> {
+    items: ItemType<T>[];
+    open: boolean;
+    placeholder?: string;
+    closeAfterSelecting?: boolean;
+    labelProps?: TextProps;
+    disabled?: boolean;
+    disabledStyle?: StyleProp<ViewStyle>;
+    placeholderStyle?: StyleProp<TextStyle>;
+    containerStyle?: StyleProp<ViewStyle>;
+    style?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
+    labelStyle?: StyleProp<TextStyle>;
+    arrowIconStyle?: StyleProp<ViewStyle>;
+    tickIconStyle?: StyleProp<ViewStyle>;
+    closeIconStyle?: StyleProp<ViewStyle>;
+    badgeStyle?: StyleProp<ViewStyle>;
+    badgeTextStyle?: StyleProp<TextStyle>;
+    badgeDotStyle?: StyleProp<ViewStyle>;
+    iconContainerStyle?: StyleProp<ViewStyle>;
+    searchContainerStyle?: StyleProp<ViewStyle>;
+    searchTextInputStyle?: StyleProp<TextStyle>;
+    searchPlaceholderTextColor?: string;
+    searchWithRegionalAccents?: boolean;
+    dropDownContainerStyle?: StyleProp<ViewStyle>;
+    modalContentContainerStyle?: StyleProp<ViewStyle>;
+    modalAnimationType?: 'none' | 'slide' | 'fade';
+    arrowIconContainerStyle?: StyleProp<ViewStyle>;
+    closeIconContainerStyle?: StyleProp<ViewStyle>;
+    tickIconContainerStyle?: StyleProp<ViewStyle>;
+    listItemContainerStyle?: StyleProp<ViewStyle>;
+    listItemLabelStyle?: StyleProp<TextStyle>;
+    listChildContainerStyle?: StyleProp<ViewStyle>;
+    listChildLabelStyle?: StyleProp<TextStyle>;
+    listParentContainerStyle?: StyleProp<ViewStyle>;
+    listParentLabelStyle?: StyleProp<TextStyle>;
+    selectedItemContainerStyle?: StyleProp<ViewStyle>;
+    selectedItemLabelStyle?: StyleProp<TextStyle>;
+    disabledItemContainerStyle?: StyleProp<ViewStyle>;
+    disabledItemLabelStyle?: StyleProp<TextStyle>;
+    customItemContainerStyle?: StyleProp<ViewStyle>;
+    customItemLabelStyle?: StyleProp<TextStyle>;
+    listMessageContainerStyle?: StyleProp<ViewStyle>;
+    listMessageTextStyle?: StyleProp<TextStyle>;
+    itemSeparatorStyle?: StyleProp<ViewStyle>;
+    badgeSeparatorStyle?: StyleProp<ViewStyle>;
+    listMode?: ListModeType;
+    categorySelectable?: boolean;
+    searchable?: boolean;
+    searchPlaceholder?: string;
+    schema?: Partial<SchemaInterface>;
+    language?: LanguageType;
+    translation?: Partial<TranslationInterface>;
+    multipleText?: string;
+    mode?: ModeType;
+    itemKey?: string;
+    maxHeight?: number;
+    renderBadgeItem?: (props: RenderBadgeItemPropsInterface<T>) => JSX.Element;
+    renderListItem?: (props: RenderListItemPropsInterface<T>) => JSX.Element;
+    itemSeparator?: boolean;
+    bottomOffset?: number;
+    badgeColors?: string[] | string;
+    badgeDotColors?: string[] | string;
+    showArrowIcon?: boolean;
+    showBadgeDot?: boolean;
+    showTickIcon?: boolean;
+    stickyHeader?: boolean;
+    autoScroll?: boolean;
+    ArrowUpIconComponent?: (props: {
+      style: StyleProp<ViewStyle>;
+    }) => JSX.Element;
+    ArrowDownIconComponent?: (props: {
+      style: StyleProp<ViewStyle>;
+    }) => JSX.Element;
+    TickIconComponent?: (props: { style: StyleProp<ViewStyle> }) => JSX.Element;
+    CloseIconComponent?: (props: {
+      style: StyleProp<ViewStyle>;
+    }) => JSX.Element;
+    ListEmptyComponent?: (
+      props: ListEmptyComponentPropsInterface,
+    ) => JSX.Element;
+    ActivityIndicatorComponent?: (
+      props: ActivityIndicatorComponentPropsInterface,
+    ) => JSX.Element;
+    activityIndicatorSize?: number;
+    activityIndicatorColor?: string;
+    props?: TouchableOpacityProps;
+    itemProps?: TouchableOpacityProps;
+    badgeProps?: TouchableOpacityProps;
+    modalProps?: ModalProps;
+    flatListProps?: Partial<FlatListProps<ItemType<T>>>;
+    scrollViewProps?: ScrollViewProps;
+    searchTextInputProps?: TextInputProps;
+    modalTitle?: string;
+    modalTitleStyle?: StyleProp<TextStyle>;
+    loading?: boolean;
+    min?: number;
+    max?: number;
+    addCustomItem?: boolean;
+    setOpen: Dispatch<SetStateValue<boolean>>;
+    setItems?: Dispatch<SetStateCallback<any[]>>;
+    disableBorderRadius?: boolean;
+    containerProps?: ViewProps;
+    onLayout?: (e: LayoutChangeEvent) => void;
+    onPress?: (open: boolean) => void;
+    onOpen?: () => void;
+    onClose?: () => void;
+    onChangeSearchText?: (text: string) => void;
+    onDirectionChanged?: (direction: DropDownDirectionType) => void;
+    zIndex?: number;
+    zIndexInverse?: number;
+    disableLocalSearch?: boolean;
+    dropDownDirection?: DropDownDirectionType;
+    theme?: ThemeNameType;
+    rtl?: boolean;
+    testID?: string;
+    closeOnBackPressed?: boolean;
+    hideSelectedItemIcon?: boolean;
+    extendableBadgeContainer?: boolean;
+  }
 
-    /**
-    * set fontFamily of whole component Text 
-    */
-    fontFamily?: string,
+  interface DropDownPickerSingleProps<T> {
+    multiple?: false;
+    onChangeValue?: (value: T | null) => void;
+    onSelectItem?: (item: ItemType<T>) => void;
+    setValue: Dispatch<SetStateCallback<T | null | any>>;
+    value: T | null;
+  }
 
-    /**
-    * set this to change the default search failure text
-    */
-    notFoundText?: string,
+  interface DropDownPickerMultipleProps<T> {
+    multiple: true;
+    onChangeValue?: (value: T[] | null) => void;
+    onSelectItem?: (items: ItemType<T>[]) => void;
+    setValue: Dispatch<SetStateCallback<T[] | null | any>>;
+    value: T[] | null;
+  }
 
-    /**
-    * Additional styles for disabled list item
-    */
-    disabledItemStyles?: ViewStyle,
+  interface DropDownPickerInterface {
+    MODE: ModeInterface;
+    LIST_MODE: ListModeInterface;
+    DROPDOWN_DIRECTION: DropDownDirectionType;
+    SCHEMA: SchemaInterface;
+    LANGUAGE: LanguageType;
+    THEMES: ThemeNameType;
+    setMode: (mode: string) => void;
+    setListMode: (mode: string) => void;
+    setDropDownDirection: (direction: DropDownDirectionType) => void;
+    setTheme: (name: string) => void;
+    addTheme: (name: string, theme: ThemeNameType) => void;
+    setLanguage: (language: string) => void;
+    addTranslation: (
+      language: string,
+      translation: TranslationInterface,
+    ) => void;
+    modifyTranslation: (
+      language: string,
+      translation: TranslationInterface,
+    ) => void;
+  }
 
-    /**
-    * Additional styles for disabled list items text
-    */
-    disabledTextStyles?: TextStyle,
+  export type DropDownPickerProps<T> = (
+    | DropDownPickerSingleProps<T>
+    | DropDownPickerMultipleProps<T>
+  ) &
+    DropDownPickerBaseProps<T>;
 
-    /**
-    * What to store inside your local state (key or value)
-    */
-    save?: 'key' | 'value',
+  const DropDownPicker: (<T extends ValueType>(
+    props: PropsWithoutRef<DropDownPickerProps<T>>,
+  ) => React.ReactElement) &
+    DropDownPickerInterface;
 
-    /**
-    * Control the dropdown with this prop
-    */
-    dropdownShown?: boolean,
-
-    /**
-    *  Pass any JSX to this prop like Text, Image or Icon to show instead of close icon
-    */
-    closeicon?: JSX.Element,
+  export default DropDownPicker;
 }
-
-
-export interface MultipleSelectListProps  {
-    /**
-    * Fn to set Selected option value which will be stored in your local state
-    */
-    setSelected: Function,
-
-    /**
-    * Placeholder text that will be displayed in the select box
-    */
-    placeholder?: string,
-
-    /**
-    * Additional styles for select box
-    */
-    boxStyles?: ViewStyle,
-
-    /**
-    *  	Additional styles for text of select box
-    */
-    inputStyles?: TextStyle,
-
-    /**
-    *  	Additional styles for dropdown scrollview 
-    */
-    dropdownStyles?:ViewStyle,
-
-    /**
-    *  Additional styles for dropdown list item
-    */
-    dropdownItemStyles?: ViewStyle,
-
-    /**
-    * Additional styles for list items text
-    */
-    dropdownTextStyles?: TextStyle,
-
-    /**
-    * Maximum height of the dropdown wrapper to occupy
-    */
-    maxHeight?: number,
-
-    /**
-    * Data which will be iterated as options of select list
-    */
-    data: Array<{}>,
-
-    /**
-    * The default option of the select list
-    */
-    defaultOption?: { key: any, value: any },
-
-    /**
-    * Pass any JSX to this prop like Text, Image or Icon to show instead of search icon
-    */
-    searchicon?: JSX.Element,
-
-    /**
-    *  Pass any JSX to this prop like Text, Image or Icon to show instead of chevron icon
-    */
-    arrowicon?: JSX.Element,
-
-    /**
-    * set to false if you dont want to use search functionality
-    */
-    search?: boolean,
-
-    /**
-    * set to false if you dont want to use search functionality
-    */
-     searchPlaceholder?: string,
-
-    /**
-    * Trigger an action when option is selected
-    */
-    onSelect?: () => void,
-
-    /**
-    * set text of label which appears soon after multiple values are selected
-    */
-    label?: string,
-
-    /**
-    * set fontFamily of whole component Text 
-    */
-    fontFamily?: string,
-
-    /**
-    * set this to change the default search failure text
-    */
-    notFoundText?: string,
-
-    /**
-    * Additional styles for disabled list item
-    */
-    disabledItemStyles?: ViewStyle,
-
-    /**
-    * Additional styles for disabled list items text
-    */
-    disabledTextStyles?: TextStyle,
-
-
-    /**
-    * Additional styles for disabled checkbox
-    */
-    disabledCheckBoxStyles?: ViewStyle,
-
-    /**
-    * Additional styles for checkbox
-    */
-    checkBoxStyles?: ViewStyle,
-
-    /**
-    * What to store inside your local state (key or value)
-    */
-    save?: 'key' | 'value',
-    
-    /**
-    * Control the dropdown with this prop
-    */
-    dropdownShown?: boolean,
-
-    /**
-    *  Pass any JSX to this prop like Text, Image or Icon to show instead of close icon
-    */
-    closeicon?: JSX.Element,
-
-    
-    /**
-    * Additional styles for multiselect badge
-    */
-    badgeStyles?: ViewStyle,
-
-    /**
-    * Additional styles for multiselect badge text
-    */
-    badgeTextStyles?: ViewStyle,
-
-    /**
-    * Additional styles for label
-    */
-    labelStyles?: TextStyle,
-}
-
-declare class MultipleSelectList extends React.Component<MultipleSelectListProps> {}
-
-declare class SelectList extends React.Component<SelectListProps> {}
-
-export {
-    MultipleSelectList,
-    SelectList
-};
